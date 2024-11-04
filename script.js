066d855c2d1d7a078d6257b97d71490e62eec17b
PraveenV2603 <praveenv.work@gmail.com>
2024-11-04 12:38:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,49 @@
-AOS.init();
-
-function showSection(sectionId) {
-    document.querySelectorAll('section').forEach(section => {
-        section.classList.remove('active');
-    });
-    document.getElementById(sectionId).classList.add('active');
+function hamburg(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(0px)"
 }
 
-function hamburg() {
-    document.querySelector('.dropdown').classList.toggle('active');
+function cancel(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(-500px)"
 }
 
-function cancel() {
-    document.querySelector('.dropdown').classList.remove('active');
-}
+// Typewriter Effect
 
-const skills = [
-    "Data Analyst",
-    "Data Scientist",
-    "Machine Learning Engineer",
-    "Data Engineer",
-    "Business Intelligence Analyst",
-    "Data Visualization Specialist",
-    "Big Data Engineer",
-    "Artificial Intelligence Engineer"
-];
+const texts = [
+    "DATA SCIENTIST",
+    "DATA ANALYST",
+    "MACHINE LEARNING ENGINEER",
+    "AI DEVELOPER",
+]
 
-let i = 0;
-let typewriterText = document.querySelector('.typewriter-text');
+let speed  =100;
+const textElements = document.querySelector(".typewriter-text");
 
-function type() {
-    if (i < skills.length) {
-        typewriterText.textContent = skills[i];
-        i++;
-        setTimeout(type, 2000);
-    } else {
-        i = 0; // Reset for looping
-        setTimeout(type, 2000);
+let textIndex = 0;
+let charcterIndex = 0;
+
+function typeWriter(){
+    if (charcterIndex < texts[textIndex].length){
+        textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
+        charcterIndex++;
+        setTimeout(typeWriter, speed); 
+    }
+    else{
+        setTimeout(eraseText, 1000)
     }
 }
 
-type();
+function eraseText(){
+    if(textElements.innerHTML.length > 0){
+        textElements.innerHTML = textElements.innerHTML.slice(0,-1);
+        setTimeout(eraseText, 50)
+    }
+    else{
+        textIndex = (textIndex + 1) % texts.length;
+        charcterIndex = 0;
+        setTimeout(typeWriter, 500)
+    }
+}
+
+window.onload = typeWriter
